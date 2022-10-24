6fefc93706fd5efd8745979b724305f25ebbc978
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-24 12:35:03 +0200

    Created PostsList component with a basic style and instantiated in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import styledMainTheme from "../../styleMainTheme";
+import PostList from "../PostList/PostList";
 
 function App() {
   return (
     <ThemeProvider theme={styledMainTheme}>
-      <div className="App"></div>
+      <div className="App">
+        <PostList />
+      </div>
     </ThemeProvider>
   );
 }
