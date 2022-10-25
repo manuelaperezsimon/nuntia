063f3f9f35ca98fa91d483e4d8b6c9d0e257b951
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-25 12:09:57 +0200

    Added the deletePost in onClick of icon trash and tested it

--- src/components/PostCard/PostCard.test.tsx
+++ src/components/PostCard/PostCard.test.tsx
@@ -3,6 +3,11 @@ import { Provider } from "react-redux";
 import { store } from "../../store/store";
 import { fakePost } from "../../test-utils/mocks/posts/postsMocks";
 import PostCard from "./PostCard";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
+
+let mockRemovePost = { deletePost: jest.fn() };
+jest.mock("../../hooks/usePosts/usePosts", () => () => mockRemovePost);
 
 describe("Given a Post Card component", () => {
   describe("When it's intantiated", () => {
@@ -25,6 +30,24 @@ describe("Given a Post Card component", () => {
       post.forEach((element) => {
         expect(element).toBeInTheDocument();
       });
+    });
+  });
+
+  describe("When it's instantiated and click on icon trash", () => {
+    test("Then it should call the remove function", async () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <PostCard post={fakePost} />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const iconTrash = screen.getByTestId("icon-trash");
+
+      await userEvent.click(iconTrash);
+
+      await expect(mockRemovePost.deletePost).toHaveBeenCalled();
     });
   });
 });
