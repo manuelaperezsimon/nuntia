93202c0dee8886a898304c70310a906c9f383faf
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-25 11:28:49 +0200

    Created the neccessary handler for delete request and tested deletePost

--- src/hooks/usePosts/usePosts.test.ts
+++ src/hooks/usePosts/usePosts.test.ts
@@ -1,10 +1,14 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
-import { loadAllPostsActionCreator } from "../../store/features/slices/posts/postsSlice";
+import {
+  deletePostActionCreator,
+  loadAllPostsActionCreator,
+} from "../../store/features/slices/posts/postsSlice";
 import usePosts from "./usePosts";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { fakeListPostsWithUserName } from "../../test-utils/mocks/posts/postsMocks";
+import { isLoadingActionCreator } from "../../store/features/slices/loading/loadingSlice";
 
 jest.mock("react-toastify");
 const mockUseDispatch = jest.fn();
@@ -18,6 +22,12 @@ describe("Given a usePosts function", () => {
   const {
     result: {
       current: { getAllPosts },
+    },
+  } = renderHook(usePosts);
+
+  const {
+    result: {
+      current: { deletePost },
     },
   } = renderHook(usePosts);
   describe("When getAllPosts it's invoked", () => {
@@ -49,6 +59,64 @@ describe("Given a usePosts function", () => {
         );
 
         delete axios.defaults.headers.get["IsTestError"];
+      });
+    });
+
+    describe("When deletePost it's invoked with a post id", () => {
+      test("Then it should call de dispatch with the deleteActionCreator with this id and the isLoadingActionCreator", async () => {
+        await act(async () => {
+          await deletePost(fakeListPostsWithUserName[0].id);
+        });
+
+        await waitFor(() => {
+          expect(mockUseDispatch).toHaveBeenCalledWith(
+            deletePostActionCreator(fakeListPostsWithUserName[0].id)
+          );
+        });
+      });
+
+      test("Then it should call the dispatch with isLoadingActionCreator", async () => {
+        await act(async () => {
+          await deletePost(fakeListPostsWithUserName[0].id);
+        });
+
+        await waitFor(() => {
+          expect(mockUseDispatch).toHaveBeenCalledWith(
+            isLoadingActionCreator()
+          );
+        });
+      });
+    });
+
+    describe("When deletePost it's invoked with a post id and the deleted was ok", () => {
+      test("Then it should call the success modal", async () => {
+        await act(async () => {
+          await deletePost(fakeListPostsWithUserName[0].id);
+        });
+
+        expect(toast.success).toHaveBeenCalledWith(
+          "Great! The post has been deleted!",
+          {
+            position: toast.POSITION.TOP_CENTER,
+          }
+        );
+      });
+    });
+
+    describe("When deletePost it's invoked with a wrong id", () => {
+      test("Then it call de errorModal", async () => {
+        const wrongId = 10;
+
+        await act(async () => {
+          await deletePost(wrongId);
+        });
+
+        expect(toast.error).toHaveBeenCalledWith(
+          "Oops, something went wrong :(",
+          {
+            position: toast.POSITION.TOP_CENTER,
+          }
+        );
       });
     });
   });
--- src/test-utils/mocks/handlers/handlers.ts
+++ src/test-utils/mocks/handlers/handlers.ts
@@ -3,6 +3,7 @@ import { fakeListPosts } from "../posts/postsMocks";
 
 const allPostsURL = "https://jsonplaceholder.typicode.com/posts";
 const user = "https://jsonplaceholder.typicode.com/users/";
+const idPost: number = 1;
 
 export const handlers = [
   rest.get(allPostsURL, async (req, res, ctx) => {
@@ -26,5 +27,13 @@ export const handlers = [
         name: "Leanne Graham",
       })
     );
+  }),
+
+  rest.delete(`${allPostsURL}/${idPost}`, async (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ postDelete: { id: idPost } }));
+  }),
+
+  rest.delete(`${allPostsURL}/10`, async (req, res, ctx) => {
+    return res(ctx.status(404), ctx.json({ error: "Post not found" }));
   }),
 ];
