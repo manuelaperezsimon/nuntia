1b8c9d2b0a0649cee525176984972c08da20d576
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-26 11:50:53 +0200

    Installed poppins font and put it in body and styledMainTheme

--- src/styleMainTheme.ts
+++ src/styleMainTheme.ts
@@ -3,6 +3,8 @@ const styledMainTheme = {
   secondaryColor: "#FBF4EE",
   blackColor: "#000000",
   whiteColor: "#FFFFFF",
+  primaryFont: "'Poppins', sans-serif",
+  secondaryFont: "'Roboto', sans-serif",
 };
 
 export default styledMainTheme;
