159e819ffd5fa409e05276bc31ff4f820f86a15e
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-26 11:51:27 +0200

    Added style in PostCard and PostsList

--- src/components/PostCard/PostCardStyled.ts
+++ src/components/PostCard/PostCardStyled.ts
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
 const PostCardStyled = styled.article`
+  display: flex;
+  flex-direction: column;
+  font-size: 1.1rem;
+  position: relative;
+  font-family: "Poppins", sans-serif;
+
   .post-card__container {
     background-color: ${(props) => props.theme.secondaryColor};
-    border: red solid 1px;
     display: grid;
     justify-content: center;
     align-items: center;
@@ -13,6 +18,11 @@ const PostCardStyled = styled.article`
   .icon {
     display: grid;
     justify-content: flex-end;
+  }
+
+  .post-card__author {
+    color: ${(props) => props.theme.primaryColor};
+    font-size: 1.3rem;
   }
 `;
 
