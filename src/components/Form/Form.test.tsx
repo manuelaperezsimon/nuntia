347eca64dda049e5be391cd231b76ddd938225b5
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-27 09:32:23 +0200

    Added functionality to form and tested it

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -3,7 +3,15 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "../../store/store";
 import { fakePost } from "../../test-utils/mocks/posts/postsMocks";
+import userEvent from "@testing-library/user-event";
 import Form from "./Form";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
 
 describe("Given a Form component", () => {
   describe("When it's instantiated", () => {
@@ -33,6 +41,52 @@ describe("Given a Form component", () => {
       form.forEach((element) => {
         expect(element).toBeInTheDocument();
       });
+    });
+  });
+  describe("When click on close icon", () => {
+    test("Then it should call the navigateToPostsList function", async () => {
+      render(
+        <Provider store={store}>
+          <Form post={fakePost} />
+        </Provider>
+      );
+
+      const iconClose = screen.getByTestId("icon-close");
+
+      await userEvent.click(iconClose);
+
+      expect(iconClose).toBeInTheDocument();
+
+      expect(mockNavigate).toHaveBeenCalled();
+    });
+  });
+
+  describe("When the user type in the inputs", () => {
+    test("Then the inputs values have to be that user type", async () => {
+      const titleInputTyped = "Hi";
+      const bodyInputTyped = "Bye";
+
+      render(
+        <Provider store={store}>
+          <Form post={fakePost} />
+        </Provider>
+      );
+
+      const placeholderInputTitle = "THE title for your post:)";
+      const placeholderInputBody = "Write your post!";
+
+      const inputOfTitle = screen.getByPlaceholderText(
+        placeholderInputTitle
+      ) as HTMLInputElement;
+      const inputOfBody = screen.getByPlaceholderText(
+        placeholderInputBody
+      ) as HTMLInputElement;
+
+      await userEvent.type(inputOfTitle, titleInputTyped);
+      await userEvent.type(inputOfBody, bodyInputTyped);
+
+      expect(inputOfTitle).toHaveValue(fakePost.title + titleInputTyped);
+      expect(inputOfBody).toHaveValue(fakePost.body + bodyInputTyped);
     });
   });
 });
