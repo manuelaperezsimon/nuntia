9b60d8bf109b2738bd4e47661790fe7e68efd418
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-28 12:52:31 +0200

    Added styles to Button

--- src/components/Button/ButtonStyled.ts
+++ src/components/Button/ButtonStyled.ts
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 
 const ButtonStyled = styled.button`
-  padding: 5px;
-  border-radius: 5px;
-  background-color: ${(props) => props.theme.whiteColor};
-  color: ${(props) => props.theme.blackColor};
+  background-color: ${(props) => props.theme.primaryColor};
+  color: ${(props) => props.theme.whiteColor};
   border: 1px solid transparent;
-  width: fit-content;
+  border-radius: 0.2rem;
+  padding: 0.7rem 1.6rem;
+  width: 100%;
   display: inline-block;
+  font-family: inherit;
+  font-weight: bold;
+  font-size: 1.1rem;
+  cursor: pointer;
   text-decoration: none;
+
+  .button--small {
+    width: 50%;
+  }
 `;
 
 export default ButtonStyled;
