192badbf5a848a609828366e68f87f11e8810995
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-28 16:00:50 +0200

    Give style to notFound component

---
+++ src/components/NotFound/NotFoundStyled.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const NotFoundStyled = styled.div`
+  margin-top: 4rem;
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3rem;
+  }
+
+  .icon--emoji {
+    font-size: 10rem;
+  }
+`;
+
+export default NotFoundStyled;
