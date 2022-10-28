b220f1c2c68f966ef392e342ae535343932e2092
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-28 15:59:33 +0200

    Changed the heading of notFound and put the button in component

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,14 +1,9 @@
-import { Link } from "react-router-dom";
-import ButtonStyled from "../../components/Button/ButtonStyled";
 import NotFound from "../../components/NotFound/NotFound";
 
 const NotFoundPage = (): JSX.Element => {
   return (
     <>
       <NotFound />
-      <Link to={"/posts"}>
-        <ButtonStyled className="button-link">Go to posts!</ButtonStyled>
-      </Link>
     </>
   );
 };
