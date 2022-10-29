8bcfcb08b4f05a588172da995b0f2c7f15bfb534
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-29 13:15:15 +0200

    Added a logout function in header component and tested it

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,22 @@
 import { BiLogOut } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import useUsers from "../../hooks/useUsers/useUsers";
 import HeaderStyled from "./HeaderStyled";
 
 const Header = (): JSX.Element => {
+  const { logout } = useUsers();
+  const navigate = useNavigate();
+
+  const logoutUser = () => {
+    logout();
+    navigate("/login");
+  };
   return (
     <HeaderStyled>
       <section className="header">
         <h1 className="header__text logo">nuntia.</h1>
         <BiLogOut
-          onClick={() => {}}
+          onClick={logoutUser}
           data-testid="icon-logout"
           className="icon-logout"
         />
