91c92cdf2d21086729449a581a0523e71041745f
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-10-31 13:02:13 +0100

    Installed cypress and tested the happy path of SPA

---
+++ cypress/e2e/nuntia.cy.ts
@@ -0,0 +1,50 @@
+import cypress from "cypress";
+
+describe("Given a URL of nuntia", () => {
+  it("Then it should visit nuntia web", () => {
+    cy.visit("https://nuntia.netlify.app/");
+    cy.get(".button").should("be.disabled");
+    cy.get("#userName").type("Bret");
+    cy.get("#password").type("hello");
+    cy.get(".button").should("not.be.disabled");
+    cy.get(".button").click();
+  });
+
+  it("Then it should visit the page of posts", () => {
+    cy.get("h2").contains("Wow, you don't understand anything, do you?");
+    cy.wait(7000);
+    cy.get(".post-card__container");
+    cy.get("button:first").click();
+  });
+
+  it("Then it should visit the edit page form", () => {
+    cy.get("h2").contains("Edit the post!");
+    cy.get("#title");
+    cy.get("#body");
+    cy.get("button").click();
+    cy.wait(5000);
+  });
+
+  it("Then it should come back to posts page", () => {
+    cy.get("h2").contains("Wow, you don't understand anything, do you?");
+    cy.get(".post-card__container");
+    cy.contains("Edit").click();
+    cy.get("h2").contains("Edit the post!");
+    cy.wait(4000);
+    cy.get(".icon--close").click();
+    cy.wait(4000);
+  });
+
+  it("Then it should come back to posts page and deleted the post selected", () => {
+    cy.get(".post-card__container");
+    cy.get(".icon-trash");
+  });
+
+  it("Then it should go to login page", () => {
+    cy.get(".icon-logout").click().clearLocalStorage();
+    cy.get("h3").contains("Log In");
+    cy.get(".button").should("be.disabled");
+    cy.get("#userName");
+    cy.get("#password");
+  });
+});
